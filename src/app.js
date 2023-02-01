add24549d9019278c8880bd27a57a2e21eea15ab
un <un@un>
2023-02-01 12:43:13 +0200

    1. add a /zaptic route that will return the Zaptic logo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const port = 3333;
 const app = express();
 
+const getDirnameInESModule = () => {
+  const __filename = fileURLToPath(import.meta.url);
+  return path.dirname(__filename);
+};
+
+const PUBLIC_DIR_PATH = getDirnameInESModule() + "/public/res/";
+const ZAPTIC_LOGO_PATH = PUBLIC_DIR_PATH + "zaptic_logo.jpeg";
+
 app.get("/", (req, res) => {
-    res.send("Hello from Zaptic! 👋💜");
+  res.send("Hello from Zaptic! 👋💜");
+});
+
+app.get("/zaptic", function (req, res) {
+  res.sendFile(ZAPTIC_LOGO_PATH); // sendFile() automatically adds the appropriate headers for the filetype
 });
 
 app.listen(port, () => {
-    console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening: http://localhost:${port}`);
 });
 
 export default app;
