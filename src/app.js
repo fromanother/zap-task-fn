a143e72f76c0ecac28f1304622d12d5b0a6ef266
un <un@un>
2023-02-01 12:59:04 +0200

    2. create error handling middleware that returns the message below when you navigate to an inexistent route

--- src/app.js
+++ src/app.js
@@ -9,16 +9,21 @@ const getDirnameInESModule = () => {
   const __filename = fileURLToPath(import.meta.url);
   return path.dirname(__filename);
 };
-
-const PUBLIC_DIR_PATH = getDirnameInESModule() + "/public/res/";
-const ZAPTIC_LOGO_PATH = PUBLIC_DIR_PATH + "zaptic_logo.jpeg";
+const PUBLIC_DIR_PATH = getDirnameInESModule() + "/public";
 
 app.get("/", (req, res) => {
   res.send("Hello from Zaptic! 👋💜");
 });
 
 app.get("/zaptic", function (req, res) {
+  const ZAPTIC_LOGO_PATH = PUBLIC_DIR_PATH + "/res/zaptic_logo.jpeg";
   res.sendFile(ZAPTIC_LOGO_PATH); // sendFile() automatically adds the appropriate headers for the filetype
+});
+
+// Because it's added at the bottom of the middleware stack, this will execute when no other routes matched,
+// so it's appropriate for handling 404
+app.use((req, res) => {
+  res.status(404).send("Seems like you're lost😱. Do you need some help?🕵️");
 });
 
 app.listen(port, () => {
